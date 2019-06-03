dbbb2fc3ec2181299934b77ef4e56c5e203cbb31
Destitution-SC <43219594+Destitution-SC@users.noreply.github.com>
2019-06-03 18:07:31 +0200

    Update circle.js

--- commands/circle.js
+++ commands/circle.js
@@ -2,7 +2,6 @@ var circle = require('@fand/circle-text')
 
 
 module.exports.run = (bot, message, args) => {
-    message.delete();
     if (!args[0]) return message.channel.send("Provide a message please.").then(msg => {msg.delete(3000)});;
 
     const sayMessage = args.join(" ");
