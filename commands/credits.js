29b859dbd105109c912cfb82aaa71f56684637e1
Destitution-SC <43219594+Destitution-SC@users.noreply.github.com>
2019-06-03 18:15:25 +0200

    Create credits.js

---
+++ commands/credits.js
@@ -0,0 +1,27 @@
+const { RichEmbed } = require("discord.js");
+
+exports.run = (bot, message) => {
+  if (!message.guild.member(bot.user).hasPermission('SEND_MESSAGES')) return;
+        
+        const user = message.author;
+
+        const credits = new RichEmbed()
+            .setTitle("Credits")
+            .setAuthor(`${user.username}`, user.displayAvatarURL)
+            .setDescription("StrongCraft Credits Information:")
+            .addField(":keyboard: | Development:","`Destitution#5128`", true)
+            .addField(":thought_balloon: | Contributeurs :", "`None`", true)       
+            .addField(":bulb: | Language :", "`JavaScript`", true)
+            .addField(":desktop: | Framework :", "`Node.js`", true)
+            .addField(":bookmark: | IDE :", "`Visual Studio Code`", true)
+            .addField(":necktie: | Adminstrators Bot :", "`Destitution#5128`", true) 
+            .setColor('RANDOM')
+            .setTimestamp();
+            
+            message.channel.send(credits);
+}
+
+exports.help = {
+    name: "credits",
+    aliases: ["credit"],
+}
