bc812ea1cd3f355f01bee541d89c6ab1158ef085
Destitution-SC <43219594+Destitution-SC@users.noreply.github.com>
2019-06-03 23:37:48 +0200

    Update setGame.js

--- commands/setGame.js
+++ commands/setGame.js
@@ -1,14 +1,22 @@
 const Discord = require('discord.js');
 
-module.exports.run = async (bot, message) => {
-    let args = message.content.split(" ").slice(1);
+module.exports.run = async (bot, message, args) => {
+if (!message.guild.member(bot.user).hasPermission('SEND_MESSAGES')) return;
+
+message.delete();
+
 let status = args.join(' ')
 if(message.author.id !== "315577349192286228") return message.channel.send("You do not have permission!");
 
-message.delete();
 bot.user.setActivity(status)
 
+    const setGameEmbed = new RichEmbed()
+        .setColor('#68f442')
+        .setTitle("Change of game status")
+        .setDescription(`New status: ${status}`)
+        .setTimestamp();
 
+    message.channel.send(setGameEmbed).then(msg => {msg.delete(8000)});;
 }
 
 module.exports.help = {
