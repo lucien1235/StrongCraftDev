4ebaf97ba1d78846d35cfb7579b1f858c5b67659
Destitution-SC <43219594+Destitution-SC@users.noreply.github.com>
2019-06-03 22:02:47 +0200

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -3,11 +3,11 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
     // 8ball Command
-    if(!args[2]) return message.channel.send(`:8ball: Please ask a full question! :8ball:`).then(msg => {msg.delete(3000)});;
+    if(!args[3]) return message.channel.send(`:8ball: Please ask a full question! :8ball:`).then(msg => {msg.delete(3000)});;
     let replies = ["Yes.", "No.", "I don't know.", "Ask again later please.", "Maybe...", "Who knows...", "Very doubtful.", "Concentrate and ask again.", "Without a doubt.", "It is decidedly so.", "My sources say no.", "Outlook good.", "Cannot predict now.", "Outlook not so good.", "It is certain.", "My reply is no.", "My reply is yes.", "Don't count on it.", "Most likely.", "You may rely on it.", "Signs point to yes.", "Needs a lot of thinking :thinking:"];
 
     let result = Math.floor((Math.random() * replies.length))
-    let question = args.slice(1).join(" ");
+    let question = args.join(" ");
     let user = message.author
     let iconURL = user.avatarURL
 
@@ -16,7 +16,7 @@ module.exports.run = async (bot, message, args) => {
     .setColor("#FF9900")
     .addField("Question :", question)
     .addField("Answer : ", replies[result])
-    .setFooter(`Requested by | ${message.author.tag}`);
+    .setFooter(`Requested by | ${message.author.username}`);
 
     message.delete();
     message.channel.send(ballembed);
