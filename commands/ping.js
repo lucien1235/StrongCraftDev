f6d4c6966bca0a6e103161eac455c4daf689b990
Destitution-SC <43219594+Destitution-SC@users.noreply.github.com>
2019-06-03 21:10:18 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,21 +1,18 @@
 const Discord = require("discord.js");
-const botconfig = require("../botconfig.json");
-const green = botconfig.green;
 
 module.exports.run = async (bot, message, args) => {
-        let msg = `${Date.now() - message.createdTimestamp}`
-        let api = `${Math.round(bot.ping)}`
-        let pingEmbed = new Discord.RichEmbed()
-        .setAuthor(message.author.tag,message.author.avatarURL)
-        .setColor(green)
-        .addField('**Time Taken :**',msg + " ms")
-        .addField('**WebSocket :**',api + " ms")
+    let msg = `${Date.now() - message.createdTimestamp}`
+    let api = `${Math.round(bot.ping)}`
+    let pingEmbed = new Discord.RichEmbed()
+        .setAuthor(message.author.tag, message.author.avatarURL)
+        .setColor("RANDOM")
+        .addField('**Time Taken :**', msg + " ms")
+        .addField('**WebSocket :**', api + " ms")
         .setTimestamp(new Date())
-        .setFooter(`Requested by ${message.author.tag}`);
+        .setFooter(`Requested by ${message.author.username}`);
 
-        message.delete().catch(O_o=>{});
-        return message.channel.send(pingEmbed).then(msg => {msg.delete(5000)});;
-
+    message.delete().catch(O_o => {});
+    return message.channel.send(pingEmbed);
 }
 
 module.exports.help = {
