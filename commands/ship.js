de9b6d303cf388710f9959d01ae1bbc950df1213
Destitution-SC <43219594+Destitution-SC@users.noreply.github.com>
2019-06-03 21:40:37 +0200

    Update ship.js

--- commands/ship.js
+++ commands/ship.js
@@ -1,46 +1,25 @@
-const discord = require ('discord.js');
-const Jimp = require(`jimp`)
+const {RichEmbed} = require("discord.js");
 
-module.exports.run = async (bot, message, args) => {
+module.exports.run = (bot, message) => {
 
-    let replies = [`Will Never Work`, `Slight Chance of Working`, `👀`, `Can Work`, `Could Work`, `High Chance of Working`, `Will Work <3`, `A Great Match <3`, `PERFECT MATCH ❤`]
-    let result = Math.floor(Math.random() * replies.length);
-    let mUser = message.mentions.users.first()
-    let mUser2 = message.mentions.users.last()
-    message.delete();
-    if(!mUser) return message.channel.send(`Please specifify two users next time!`).then(msg => {msg.delete(3000)});;
-    const mUserA = mUser.avatarURL
-    var imagetobase = `https://cdn.discordapp.com/attachments/517807874353790977/584465527431495694/Shipping.png`;
-    Jimp.read(mUser.avatarURL, function (err, imagetouse) {
-        if (err) throw err;
-        imagetouse.quality(60)
-                  .resize(866, 866)
-                  .write("imagetouse.jpg");
-        Jimp.read(mUser2.avatarURL, function (err, imagetouse2) {
-        if (err) throw err;
-        imagetouse2.quality(60)
-                  .resize(866, 866)
-                  .write("imagetouse2.jpg");
-        Jimp.read(imagetobase, function (err, mydude) {
-            if (err) throw err;
-            Jimp.loadFont(Jimp.FONT_SANS_64_WHITE).then(function (font) {
-                mydude.quality(60)
-                mydude.print(font, 635, 1717, `Linked by ${message.author.username}`)
-                mydude.composite( imagetouse, 784, 797 )
-                mydude.composite( imagetouse2, 2191, 797)
-                mydude.write("saveme.jpg")
-                mydude.getBuffer(`image/jpeg`, (err, buf) => {
-                    if (err) return err;
-                    message.delete();
-                    message.channel.send({files: [{attachment: buf, name: `saveme.jpg`}] })
-                    message.channel.send(replies[result])
-                })
-            })
-        })}
-    )})
+    if (!message.guild.member(bot.user).hasPermission('SEND_MESSAGES')) return;
+    if (message.mentions.users.size !== 2) return message.channel.send("You must mention two people!")
+
+    let user1 = message.mentions.users.first();
+    let user2 = message.mentions.users.last();
+    let member1 = message.guild.member(user1);
+    let member2 = message.guild.member(user2);
+    let number = Math.floor(Math.random() * 99) + 1;
+    
+    let LoveCountAmount = new RichEmbed()
+    .setColor("#e053cb")
+    .setTitle(`**${number}% / 100%** :heart:`);
+    message.channel.send("\n:heartpulse: MATCHMAKING :heartpulse:\n*__Declare your love...__*\n :small_red_triangle_down:" + "``" + member1.user.username + "``\n :small_red_triangle:" + "``" + member2.user.username + "``\n");
+    message.channel.send(LoveCountAmount)
 
 }
+
 module.exports.help = {
     name: "ship",
-    aliases: ["love"],
+    aliases: ["lovecheck", "love", "lovecount"],
 }
